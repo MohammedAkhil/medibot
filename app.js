4758bfca141e15ecb1d3c5559924189bc4193211
Akhil Mohammed <mohammed.akhil@gyanmatrix.com>
2018-08-06 15:44:12 +0530

    add app.js with basic express setup

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+const config = require('./config/').default;
+const port = config.port;
+const bodyParser = require('body-parser');
+import { Database } from './db';
+
+app.use(express.static(__dirname + './public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// all routes must be defined here.
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+// If no route found, it must be redirected to 404
+app.use((req, res, next) => {
+  let err = { message: 'Not found', path: req.url };
+  res.status(404);
+  res.json(err);
+});
+
+/**
+ * Catching the JS error in the program
+ */
+app.use((err, req, res, next) => {
+  // then let the app to crash,
+  // because this programmer error, should be fixed immediately
+  throw err;
+});
+
+app.listen(port);
+
+module.exports = db;
